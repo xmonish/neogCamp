d7afd7cd0fbcedb1fcc7dddb148f8d16e8d8c96a
Monish Singh <36487698+xmonish@users.noreply.github.com>
2020-12-02 01:39:21 +0530

    Update index.js

--- project1/index.js
+++ project1/index.js
@@ -1,43 +1,49 @@
 var readlineSync = require('readline-sync');
-var  score = 0
-var userName = readlineSync.question("May I have your name: ");
-console.log('Welcome ' + userName + '!');
-console.log("Lets test how much you know Monish! ")
 
-function play(question,answer){
-  console.log(question)
+const chalk = require('chalk');
+const log = console.log;
+
+var score = 0
+
+var userName = readlineSync.question("Enter your name: ");
+log(chalk.keyword('orange')('Welcome ' + userName + '!'));
+log(chalk("Lets test how much do you know ") + chalk.green("Monish"))
+
+function play(question, answer) {
+  log(chalk.black('----------------------------------'))
+  log(chalk.bgRed.bold(question));
   userAnswer = readlineSync.question()
-  if(userAnswer.toUpperCase() == answer.toUpperCase() ){
-    score+=1
-    console.log("You're Right!")
+  if (userAnswer.toUpperCase() == answer.toUpperCase()) {
+    score += 1
+    log(chalk.blue("You're Right!"))
   }
-  else{
-    score-=1
-    console.log("You're Wrong")
+  else {
+    score -= 1
+    log(chalk.yellow("You're Wrong"))
   }
-  console.log("Current score: ",score)
-  console.log("----------------")
+  console.log("Current score: ", score)
 }
 
 // The toUpperCase() method converts a string to uppercase letters, but doesn't change the original string.
 
 var questionArray = [{
-    question:"Where do i live? ",
-    answer:"bhilai"
-  },{
-    question:"Where do i study? ",
-    answer:"ssgi"
-  },{
-    question:"Which game do I used to like the most? ",
-    answer:"pubg"
-  }
+  question: "Where do i live? ",
+  answer: "bhilai"
+}, {
+  question: "Where do i study? ",
+  answer: "ssgi"
+}, {
+  question: "Which game do I used to like the most? ",
+  answer: "pubg"
+}
 ]
 
-var i 
-for(i in questionArray){
+var i
+for (i in questionArray) {
   var currentQuestion = questionArray[i]
   play(currentQuestion.question, currentQuestion.answer)
 }
 
-console.log("Your final score is: ",score)
-console.log("Thanks for visiting!")
+log(chalk.black('----------------------------------'))
+log(chalk.red.underline.bold("Your final score is: ", score))
+log(chalk.hex('#DEADED').bold('Thanks for visiting!'))
